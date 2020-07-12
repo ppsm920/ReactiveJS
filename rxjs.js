9bbb48d51e926f1b57eb370f98c41c1b37ceb380
Sumin Kim <suminkim@Sumins-Air.lan>
2020-07-12 11:22:19 -0700

    finish ex 9

--- rxjs.js
+++ rxjs.js
@@ -1,4 +1,4 @@
-const newReleases = [
+const movieLists = [
   {
     id: 70111470,
     title: 'Die Hard',
@@ -27,7 +27,6 @@ const newReleases = [
     id: 675465,
     title: 'Fracture',
     boxart: 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
-
     uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
     rating: 5.0,
     bookmark: [{ id: 432534, time: 65876586 }],
@@ -36,9 +35,20 @@ const newReleases = [
 
 // Exercise 8: Chain filter and map to collect the ids of videos that have a rating of 5.0
 function exercise8() {
-  return newReleases
+  return movieLists
     .filter((video) => (video.rating === 5.0))
     .map((video) => video.id);
 }
 
-console.log(exercise8(newReleases));
+console.log(exercise8(movieLists));
+
+// Exercise 9: Flatten the movieLists array into an array of video ids
+function exercise9() {
+  const allVideoIdsInMovieLists = [];
+  movieLists.forEach((movieList) => {
+    movieList.videos.forEach((video) => { allVideoIdsInMovieLists.push(video.id); });
+  });
+  return allVideoIdsInMovieLists;
+}
+
+console.log(exercise9(movieLists));
