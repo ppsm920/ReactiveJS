c6246a3a1ce3b7115bd167cc57cde4082b8b7e5c
Sumin Kim <ppsm920@gmail.com>
2020-07-12 16:18:52 -0700

    finish ex11

---
+++ exercise 11.js
@@ -0,0 +1,11 @@
+// exercise 11 Let's use our new reduce function to isolate the largest value in an array of ratings
+function exercise11() {
+  const ratings = [2, 3, 1, 4, 5];
+  return ratings.reduce((largestNum, rating) => {
+    // eslint-disable-next-line no-param-reassign
+    if (largestNum < rating) largestNum = rating;
+    return largestNum;
+  }, ratings[0]);
+}
+
+console.log(exercise11());
