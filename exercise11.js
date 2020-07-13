ad541dc3ed6e91acbff547526d0c95bbfe8163a8
Sumin Kim <ppsm920@gmail.com>
2020-07-13 16:40:57 -0700

    add another ex

--- exercise11.js
+++ exercise11.js
@@ -9,3 +9,23 @@ function exercise11() {
 }
 
 console.log(exercise11());
+
+// exercise 12 : You should return an array containing only the URL of the largest box art.
+function exercise12() {
+  const boxarts = [
+    { width: 200, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/Fracture200.jpg' },
+    { width: 150, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/Fracture150.jpg' },
+    { width: 300, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/Fracture300.jpg' },
+    { width: 425, height: 150, url: 'http://cdn-0.nflximg.com/images/2891/Fracture425.jpg' },
+  ];
+  let multiplyResult;
+  const result = boxarts.map((boxart) => (boxart.width * boxart.height));
+  boxarts.forEach((boxart) => {
+    if (Math.max(...result) === boxart.width * boxart.height) {
+      multiplyResult = boxart.url;
+    }
+  });
+  return multiplyResult;
+}
+
+console.log(exercise12());
